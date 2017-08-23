ebd0b247b6c02c6d6702fd665749d0ac808cba33
fujun-iri <fujun-iri@360.cn>
2017-08-23 20:43:27 +0800

    boilerplate code

--- build/cmd.js
+++ build/cmd.js
@@ -9,14 +9,22 @@ if (argv.length === 0) {
 const path = require('path')
 const opn = require('opn')
 const shell = require('shelljs')
-const { log, toLog, setNodeEnv, findAvailablePort } = require('./util')
+const {
+  log,
+  toLog,
+  setNodeEnv,
+  findAvailablePort,
+  kebabCase,
+  pascalCase
+} = require('./util')
 
 const markdown = require('./markdown')
 const rollup = require('./rollup')
 const nuxtServe = require('./nuxt.server')
+const COMPONENT_DIR = path.join(__dirname, '../src/components')
 const BIN = path.join(__dirname, '../node_modules/.bin')
 const PORT = 3000
-const [ cmdName ] = argv
+const [ cmdName, ...args ] = argv
 
 const startDevServer = () => findAvailablePort(PORT)
   .then(toLog('Serving with nuxt.js...'))
@@ -26,7 +34,7 @@ const startDevServer = () => findAvailablePort(PORT)
   })
 
 switch (cmdName) {
-  case 'build':
+  case '--build':
     Promise
       .resolve(setNodeEnv('production'))
       .then(toLog('Start rollup bundling...'))
@@ -35,7 +43,7 @@ switch (cmdName) {
       .catch(log)
     break
 
-  case 'dev':
+  case '--dev':
     Promise
       .resolve(setNodeEnv('development'))
       .then(toLog('Watching markdown documents...'))
@@ -54,7 +62,7 @@ switch (cmdName) {
       .catch(log)
     break
 
-  case 'deploy':
+  case '--deploy':
     Promise
       .resolve(setNodeEnv('production'))
       .then(toLog('Building documentation...'))
@@ -68,21 +76,90 @@ switch (cmdName) {
       .catch(log)
     break
 
-  case 'clean':
+  case '--clean':
     shell.rm('-rf', '.nuxt', 'site', 'dist')
     break
-  // case 'add':
-  //   Promise
-  //     .resolve(argv[1])
-  //     .then(name => [camelCase(name), name])
-  //     .then(([compName, dirName]) => {
-  //       console.log(compName, dirName)
-  //     })
-  //   break
+  case '--add':
+    Promise
+      .resolve(args.map(n => [kebabCase(n), pascalCase(n)]))
+      .then(createBoilerplate)
+    break
 }
 
 function execAsPromise (cmd) {
   return new Promise((resolve, reject) => {
     shell.exec(cmd, resolve)
   })
+}
+
+function createBoilerplate (arr) {
+  arr.forEach(([dirName, compName]) => {
+    const dir = path.join(COMPONENT_DIR, dirName)
+    const file = path.join(dir, 'index')
+    const tagName = `c-${dirName}`
+
+    if (!shell.test('-d', dir)) {
+      shell.mkdir('-p', dir)
+
+      shell.ShellString(mdTemplate(tagName, compName)).to(`${file}.md`)
+      shell.ShellString(jsTemplate(tagName, compName)).to(`${file}.js`)
+      shell.ShellString(cssTemplate(tagName, compName)).to(`${file}.css`)
+      shell.ShellString(vueTemplate(tagName, compName)).to(`${file}.vue`)
+      log(`[DONE] ${dirName} created at ${dir}.`)
+    } else {
+      // eslint-disable no-console
+      console.warn(`[Warning] Skipping component ${dirName}, it already exisits at ${dir}.`)
+    }
+  })
+}
+
+function mdTemplate (tagName, compName) {
+  return `# ${compName}
+
+## Demo
+
+\`\`\`html
+<${tagName}></${tagName}>
+\`\`\`
+`
+}
+
+function jsTemplate (tagName, compName) {
+  return `import ${compName} from './index.vue'
+
+${compName}.install = Vue => {
+  Vue.component(${compName}.name, ${compName})
+}
+
+export default ${compName}
+`
+}
+
+function cssTemplate (tagName, compName) {
+  return `/**
+ * ${tagName}
+ */
+`
+}
+
+function vueTemplate (tagName, compName) {
+  return `<template lang="pug">
+  .${tagName}
+    | ${compName}
+</template>
+
+<script>
+
+export default {
+  name: '${tagName}',
+  props: {},
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+`
 }
