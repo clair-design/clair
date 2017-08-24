22dae46e3bb67744d565e33766cafe2a2cacb61b
webzhao <webzhao@gmail.com>
2017-08-24 19:58:03 +0800

    extract common header

--- document/plugins/clair.js
+++ document/plugins/clair.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import '../assets/clair.css'
 import Clair from '../assets/clair'
+import Header from '../layouts/includes/header.vue'
 
 Vue.use(Clair)
+Vue.component('c-header', Header)
