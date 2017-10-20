ce3cbc5249f9dbb35d3199179ca3e4c3a28a7167
webzhao <webzhao@gmail.com>
2017-10-20 20:45:12 +0800

    add icon component

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import '../css/main.css'
 
 // importing components
 import Button from '../components/button/index.vue'
+import Icon from '../components/icon/index.vue'
 
 const install = (Vue, component) => {
   Vue.component(component.name, component)
@@ -11,6 +12,7 @@ const Clair = {
   install (Vue) {
     // installing components
     install(Vue, Button)
+    install(Vue, Icon)
   }
 }
 
