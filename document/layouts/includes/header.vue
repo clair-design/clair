989d8198409fef72528cb9b98a899ec6f270d5e6
webzhao <webzhao@gmail.com>
2017-10-24 11:56:03 +0800

    fix navbar item active state

--- document/layouts/includes/header.vue
+++ document/layouts/includes/header.vue
@@ -9,6 +9,7 @@ header.c-row.toolbar
       v-for="nav in navs",
       :to="nav.link",
       :key="nav.title"
+      :class="{'is-active': nav.isActiveReg.test($route.path)}"
     ) {{nav.title}}
   a(href="https://github.com/clair-design/clair")
     c-icon.large(type="github")
@@ -16,10 +17,10 @@ header.c-row.toolbar
 
 <script>
 const navigation = [
-  { title: '首页', link: '/' },
-  { title: '设计原则', link: '/principle/introduction' },
-  { title: '组件', link: '/component/install' },
-  { title: '资源', link: '/resource' }
+  { title: '首页', link: '/', isActiveReg: /^\/$/ },
+  { title: '设计原则', link: '/principle/introduction', isActiveReg: /^\/principle/ },
+  { title: '组件', link: '/component/install', isActiveReg: /^\/component/ },
+  { title: '资源', link: '/resource', isActiveReg: /^\/resource/ }
 ]
 export default {
   data () {
