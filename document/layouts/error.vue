7e45de9cab908c46b18ff2c185b2a4cc6ccb3561
webzhao <webzhao@gmail.com>
2017-10-24 11:55:03 +0800

    update error page style

--- document/layouts/error.vue
+++ document/layouts/error.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
+  <div class="error-container">
+    <h1 v-if="error.statusCode === 404">404 无法找到请求的内容</h1>
+    <h1 v-else>加载内容失败</h1>
+    <c-button href="/principle/introduction" primary>
+      <c-icon type="home"></c-icon>
+      返回首页
+    </c-button>
   </div>
 </template>
 
