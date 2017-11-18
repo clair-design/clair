bc3caf5246a144cb5dcef2229c5605473a8817ae
webzhao <webzhao@gmail.com>
2017-11-18 11:06:00 +0800

    add layout container

---
+++ src/components/grid/container.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .c-container(:class="classNames")
+    slot
+</template>
+
+<script>
+export default {
+  name: 'c-container',
+  props: {
+    size: String,
+    align: String
+  },
+  computed: {
+    classNames () {
+      const classNames = []
+      if (this.size) classNames.push(`is-${this.size}`)
+      if (this.align) classNames.push(`is-${this.align}`)
+      return classNames
+    }
+  }
+}
+</script>
