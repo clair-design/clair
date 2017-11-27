82ac64dd3900168e78ae115464e1220113af98dd
webzhao <webzhao@gmail.com>
2017-11-27 19:05:51 +0800

    :wheelchair: improve a11y for homepage

--- docs/layouts/includes/header.vue
+++ docs/layouts/includes/header.vue
@@ -17,7 +17,11 @@ c-box.toolbar.c-container.no-gap(align="center")
           :class="{'is-active': nav.isActiveReg.test($route.path)}"
         ) {{nav.title}}
   c-box-item.is-text-right(xs="12" sm="1")
-    a.xs-12.sm-a(href="https://github.com/clair-design/clair" target="_blank")
+    a.xs-12.sm-a(
+      href="https://github.com/clair-design/clair"
+      aria-label="github repository"
+      target="_blank"
+    )
       c-icon.is-text-xl(name="github")
 </template>
 
