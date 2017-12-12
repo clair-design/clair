48d380cd8705e8f7a3ad7f6c9725c7c9ac04b518
webzhao <webzhao@gmail.com>
2017-12-12 15:51:45 +0800

    :bug: fix select destroy error

--- src/components/select/index.vue
+++ src/components/select/index.vue
@@ -254,10 +254,6 @@ export default {
     this.menuEl.remove()
   },
 
-  destroyed () {
-    this.menuEl && document.body.removeChild(this.menuEl)
-  },
-
   methods: {
     toggleOpen () {
       if (this.isOpen) {
