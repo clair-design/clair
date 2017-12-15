348fb160d2158abb4d36b2bb658adf4843fabf16
webzhao <webzhao@gmail.com>
2017-12-15 09:19:45 +0800

    :sparkle: add toolbar component

---
+++ src/components/toolbar/toolbar-item.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .c-toolbar__item(:class="{'is-flex': flex}")
+    slot
+</template>
+
+<script>
+  export default {
+    props: {
+      flex: Boolean
+    },
+    name: 'c-toolbar-item'
+  }
+</script>
+
