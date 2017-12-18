5f69da1511056a6e7e5e58f36c1949f6d0ae1edc
webzhao <webzhao@gmail.com>
2017-12-18 17:34:58 +0800

    :sparkle: add card component

---
+++ src/components/card/index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.c-card(:class="{'is-horizontal': horizontal}")
+  .c-card__title(v-if="$slots.title")
+    slot(name="title")
+  .c-card__media(v-if="$slots.media")
+    slot(name="media")
+  .c-card__body(v-if="$slots.default")
+    slot
+  .c-card__actions(v-if="$slots.actions")
+    slot(name="actions")
+</template>
+
+<script>
+import './index.css'
+
+export default {
+  name: 'c-card',
+  props: {
+    horizontal: Boolean
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
