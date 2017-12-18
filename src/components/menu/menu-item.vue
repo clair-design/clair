0961b9ef6e8a0b53032648528d2589571fcd37bc
webzhao <webzhao@gmail.com>
2017-12-18 10:22:35 +0800

    :sparkle: add menu component

---
+++ src/components/menu/menu-item.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.c-menu__item(:class="classNames")
+  slot
+</template>
+
+<script>
+export default {
+  name: 'c-menu-item',
+  props: {
+    mode: String,
+    active: Boolean
+  },
+  data () {
+    return {
+      isActive: false
+    }
+  },
+  computed: {
+    classNames () {
+      const classNames = []
+      if (this.isActive) classNames.push('is-active')
+      return classNames
+    }
+  },
+  mounted () {
+    if (this.active) this.isActive = true
+  },
+  methods: {}
+}
+</script>
