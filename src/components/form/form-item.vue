4c2c1334cb76c6d2c8faadb12f4f7c41540cbdf2
webzhao <webzhao@gmail.com>
2017-12-19 15:57:12 +0800

    :sparkle: add form component

---
+++ src/components/form/form-item.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+.c-form-item(:class="classNames")
+  label.c-form-item__label(:style="labelStyle")
+    slot(name="label") {{ label }}
+  .c-form-item__control
+    slot
+    .c-form-item__error(:style="errorStyle")
+      | {{ errorMsg }}
+</template>
+
+<script>
+export default {
+  name: 'c-form-item',
+  provide () {
+    return {
+      '$formItem': this
+    }
+  },
+  inject: {
+    $form: { default: null }
+  },
+  props: {
+    label: String,
+    required: Boolean,
+    flex: Boolean,
+    labelWidth: String
+  },
+  data () {
+    return {
+      validatable: null
+    }
+  },
+  computed: {
+    hasError () {
+      const { validatable } = this
+      return validatable && !validatable.validity.valid
+    },
+    errorStyle () {
+      const visibility = this.hasError ? 'visible' : 'hidden'
+      return { visibility }
+    },
+    errorMsg () {
+      return this.hasError ? this.validatable.validity.msg : ''
+    },
+    classNames () {
+      const classNames = []
+      if (this.required) classNames.push('is-required')
+      if (this.flex) classNames.push('is-flex')
+      return classNames
+    },
+    actualLabelWidth () {
+      if (this.labelWidth) return this.labelWidth
+      return this.$form && this.$form.labelWidth
+    },
+    labelStyle () {
+      return {
+        width: this.actualLabelWidth
+      }
+    }
+  },
+  watch: {
+    validatable () {
+      if (this.validatable && this.required) {
+        this.validatable.rules.required = true
+      }
+    }
+  },
+  created () {
+    this.$on('validatable-attached', v => {
+      this.validatable = v
+    })
+    this.$on('validatable-detached', v => {
+      this.validatable = null
+    })
+  },
+  methods: {}
+}
+</script>
