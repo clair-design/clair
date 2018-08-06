c94d396c492b475cefb8c9cb0f3baa6bd2d2010f
wemlion <angusfu1126@qq.com>
2018-08-06 16:02:46 +0800

    chore(lint): use eslint-config-clair

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,20 @@
-// https://eslint.org/docs/user-guide/configuring
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true
+  },
   parserOptions: {
     parser: 'babel-eslint'
   },
-  env: {
-    browser: true,
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+  'extends': [
+    // wrapper for `plugin:vue/essential` and `standard`
+    // SEE https://github.com/clair-design/eslint-config-clair
+    'clair'
   ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
   rules: {
-    // "no-magic-numbers": "off",
-    // allow async-await
     'generator-star-spacing': 'off',
-    // allow debugger during development
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
