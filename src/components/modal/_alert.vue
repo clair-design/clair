9cfcb4c0d6c48401a4eee5a51e480986727ff454
wemlion <angusfu1126@qq.com>
2018-08-10 14:29:30 +0800

    revert(modal): revert _alert and _message

--- src/components/modal/_alert.vue
+++ src/components/modal/_alert.vue
@@ -1,10 +1,22 @@
+
+<template lang="pug">
+c-modal(
+  :title="title",
+  :visible="visible",
+  width="400px",
+  @close="onCancel",
+  @after-leave="$emit('destroy')"
+)
+  div
+    slot(name="message")
+  div(slot="footer")
+    c-button(primary @click="onConfirm" autofocus) 确定
+</template>
+
 <script>
 export default {
   props: {
-    title: {
-      type: String,
-      default: '提示'
-    },
+    title: String,
     msg: {
       type: [String, Function],
       require: true
@@ -23,33 +35,18 @@ export default {
     onConfirm () {
       this.visible = false
       this.$emit('confirm')
-    },
-    destroyVM () {
-      this.$emit('destroy')
     }
   },
-  render (h) {
-    const { onCancel, onConfirm, destroyVM } = this
-    const button = h('c-button', {
-      on: { click: onConfirm },
-      attrs: {
-        primary: true,
-        autofocus: true
+  watch: {
+    msg: {
+      immediate: true,
+      handler () {
+        const { msg } = this
+        const h = this.$createElement.bind(this)
+        const message = typeof msg === 'function' ? msg(h) : h('span', null, msg)
+        this.$slots.message = message
       }
-    }, '确定')
-
-    const { title, msg, visible } = this
-    const attrs = { title, visible, width: '400px' }
-    const message = typeof msg === 'function' ? msg(h) : msg
-    const handlers = {
-      close: onCancel,
-      'after-leave': destroyVM
     }
-
-    return h('c-modal', { attrs, on: handlers }, [
-      h('div', null, [message]),
-      h('div', { slot: 'footer' }, [button])
-    ])
   }
 }
 </script>
