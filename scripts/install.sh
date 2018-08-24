8f6d12e8c9a01fdd1f3c06788229398fa2e3791f
wemlion <angusfu1126@qq.com>
2018-08-24 10:24:09 +0800

    ci: publish documentation exactly on release success

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,1 @@
-npm install && npm install --no-save travis-deploy-once @semantic-release/changelog @semantic-release/git @semantic-release/github @semantic-release/npm semantic-release
+npm install && npm install --no-save travis-deploy-once semantic-release @semantic-release/changelog @semantic-release/git @semantic-release/github @semantic-release/npm @semantic-release/exec
---
+++ scripts/publish-doc.sh
@@ -0,0 +1,1 @@
+npx gh-pages -r https://"$GH_TOKEN"@github.com/clair-design/clair-design.github.io.git -b master -d .site && echo "Site published: https://clair-design.github.io"
