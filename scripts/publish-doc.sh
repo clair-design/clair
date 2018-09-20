f66ef3362d702c2515959b75b6cc25c5de7d5c11
wemlion <angusfu1126@qq.com>
2018-09-20 13:30:39 +0800

    ci(publish): tagging docs

--- scripts/publish-doc.sh
+++ scripts/publish-doc.sh
@@ -1,1 +1,8 @@
-npx gh-pages -r https://"$GH_TOKEN"@github.com/clair-design/clair-design.github.io.git -b master -d .site && echo "Site published: https://clair-design.github.io"
+tagname=`git describe --tags`
+npx gh-pages \
+  --repo https://"$GH_TOKEN"@github.com/clair-design/clair-design.github.io.git \
+  --branch master \
+  --dist .site \
+  --tag $tagname
+
+echo "Site published: https://clair-design.github.io"
