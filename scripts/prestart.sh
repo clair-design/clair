8e17aa6912cc1af3dcda06edc0bdc6528951eadd
wemlion <angusfu1126@qq.com>
2018-11-28 22:52:40 +0800

    ci: update configs

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -4,3 +4,14 @@ msg="Running \`git pull\` before start is a good habit."
 echo "\033[33m${msg}\033[0m"
 echo ""
 git pull
+
+DIR_NAME=${0%/*}
+
+while true; do
+  read -p "Do you want to sync local repo with upstream?" yn
+  case $yn in
+    [Yy]* ) ${DIR_NAME}/sync-upstream.sh; break;;
+    [Nn]* ) exit;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
