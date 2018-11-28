0a0634c12950585b00bb39d3b70c0ea9595d85f8
wemlion <angusfu1126@qq.com>
2018-11-28 22:07:22 +0800

    style: fit eslint rules

--- src/components/tabs/index.vue
+++ src/components/tabs/index.vue
@@ -154,6 +154,8 @@ export default {
         [navWrapper, contentWrapper]
       )
     }
+
+    return null
   }
 }
 </script>
--- src/components/tabs/tab-bar.vue
+++ src/components/tabs/tab-bar.vue
@@ -38,6 +38,7 @@ export default {
           transform: `translateY(${this.barOffset}px)`
         }
       }
+      return null
     }
   },
 
