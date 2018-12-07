048c6acab32027709e2448c4335a2282f6ed4e5a
wemlion <angusfu1126@qq.com>
2018-12-07 22:40:00 +0800

    fix: expose featherIcons to Vue prototype

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import featherIcons from 'feather-vue'
+
 // plugins
 import Components from './components'
 
@@ -14,8 +16,22 @@ const mixins = { validatable, resettable }
 export const Clair = {
   mixins,
   install (Vue) {
+    const VuePrototype = Vue.prototype
+    const defineReadOnly = function (key, val) {
+      Object.defineProperty(VuePrototype, key, {
+        get () {
+          return val
+        },
+        configurable: process.env.NODE_ENV !== 'production',
+        enumerable: false
+      })
+    }
+
+    // set a noop utility
+    defineReadOnly('noop', _ => _)
+
     // inject $clair to Vue prototype
-    if (!('$clair' in Vue.prototype)) {
+    if (!('$clair' in VuePrototype)) {
       const $clair = new Vue({
         data: {
           responsive: null,
@@ -23,18 +39,15 @@ export const Clair = {
         }
       })
       $clair.mixins = mixins
-
-      Object.defineProperty(Vue.prototype, '$clair', {
-        get () {
-          return $clair
-        }
-      })
+      defineReadOnly('$clair', $clair)
     }
 
-    Vue.prototype.noop = () => {}
+    // expose featherIcons for convenience
+    defineReadOnly('$featherIcons', featherIcons)
 
     // register components
     Vue.use(Components)
+
     // install plugins
     Vue.use(Modal)
     Vue.use(Responsive)
