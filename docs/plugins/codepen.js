6261a1bed1eb6345771dee63d56fd1e20a484482
wemlion <angusfu1126@qq.com>
2019-02-18 17:59:48 +0800

    docs: add codesandbox support for code blocks

--- docs/plugins/codepen.js
+++ docs/plugins/codepen.js
@@ -1,3 +1,4 @@
+import { getParameters } from 'codesandbox/lib/api/define'
 import CodePen from './components/codepen.vue'
 import tinyCopy from './scripts/tiny-copy'
 
@@ -11,13 +12,18 @@ export default {
 
     const codepenVm = new Vue({
       render (h) {
+        const { type, config } = this
         return h('c-codepen', {
           props: {
-            config: JSON.stringify(this.config)
+            type,
+            config: type === 'codesandbox'
+              ? getCodesandboxParameter(config)
+              : JSON.stringify(config)
           }
         })
       },
       data: {
+        type: 'codepen',
         // see https://blog.codepen.io/documentation/api/prefill/
         config: {
           title: 'Clair Demo',
@@ -36,11 +42,12 @@ export default {
           tinyCopy(node)
         }
       },
-      open (e) {
+      open (e, type) {
         const node = getPreDOM(e)
 
         if (node) {
-          const config = node.__c || (node.__c = extractVue(node.textContent))
+          const config = extractVue(node.textContent, type)
+          codepenVm.type = type === 'codesandbox' ? 'codesandbox' : 'codepen'
           codepenVm.config = Object.assign({}, codepenVm.config, config)
           codepenVm.$nextTick(() => codepenVm.$el.submit())
         }
@@ -53,52 +60,6 @@ export default {
       })
     }
 
-    function extractVue (code) {
-      const styleMatch = reStyle.exec(code)
-      const scriptMatch = reScript.exec(code)
-      const templateMatch = reTemplate.exec(code)
-
-      const style = styleMatch ? styleMatch[1].trim() : ''
-      let script = scriptMatch ? scriptMatch[1].trim() : ''
-      let template = templateMatch ? templateMatch[2].trim() : ''
-
-      // case where `<template>` absent
-      if (template === '') {
-        template = code.replace(reStyle, '').replace(reScript, '')
-      }
-
-      if (script === '') {
-        script = 'var data = {}'
-      } else {
-        script = script.replace(/export\s+default/, 'var data =')
-      }
-
-      script += `\nnew Vue(data).$mount('#app')`
-      template = `<div id="app">
-    ${template.split('\n').join('\n  ')}
-    </div>`
-
-      return {
-        js: script,
-        css: style,
-        html: template
-      }
-    }
-
-    function getPreDOM (e) {
-      let node = e.target
-
-      while (node) {
-        if (node.classList.contains('vue-demo-block')) {
-          node = node.querySelector('.vue-demo-source-code')
-          break
-        }
-        node = node.parentNode
-      }
-
-      return node
-    }
-
     function mountCodepenVm () {
       if (typeof document !== 'undefined' && document.body) {
         const div = document.createElement('div')
@@ -107,4 +68,98 @@ export default {
       }
     }
   }
+}
+
+function extractVue (code, type) {
+  const styleMatch = reStyle.exec(code)
+  const scriptMatch = reScript.exec(code)
+  const templateMatch = reTemplate.exec(code)
+
+  const style = styleMatch ? styleMatch[1].trim() : ''
+  let script = scriptMatch ? scriptMatch[1].trim() : ''
+  let template = templateMatch ? templateMatch[2].trim() : ''
+
+  // case where `<template>` absent
+  if (template === '') {
+    template = code.replace(reStyle, '').replace(reScript, '').trim()
+  }
+
+  if (type !== 'codesandbox') {
+    if (script === '') {
+      script = 'var data = {}'
+    } else {
+      script = script.replace(/export\s+default/, 'var data =').trim()
+    }
+    script += `\nnew Vue(data).$mount('#app')`
+  }
+
+  template = `<div id="app">
+  ${template.split('\n').join('\n  ')}
+</div>`
+
+  return {
+    js: script,
+    css: style,
+    html: template
+  }
+}
+
+function getPreDOM (e) {
+  let node = e.target
+
+  while (node) {
+    if (node.classList.contains('vue-demo-block')) {
+      node = node.querySelector('.vue-demo-source-code')
+      break
+    }
+    node = node.parentNode
+  }
+
+  return node
+}
+
+function getCodesandboxParameter (config) {
+  return getParameters({
+    files: {
+      'package.json': {
+        content: {
+          dependencies: {
+            vue: 'latest',
+            clair: 'latest'
+          }
+        }
+      },
+      'index.html': {
+        content: `
+<link rel="stylesheet" href="https://lib.baomitu.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<div id="root"></div>
+`
+      },
+      'src/main.js': {
+        content: `import Vue from 'vue';
+import Clair from 'clair';
+import 'clair/dist/clair.css';
+import App from './App.vue';
+
+Vue.use(Clair);
+
+new Vue({ render: h => h(App) }).$mount('#root');`
+      },
+      'src/App.vue': {
+        content: `<template>
+${config.html}
+</template>
+
+<script>
+${config.js || 'export default {};'}
+</script>
+
+<style>
+${config.css || '/* add your CSS here */'}
+</style>
+
+`
+      }
+    }
+  })
 }
