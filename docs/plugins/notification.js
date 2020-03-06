5e237890701205ca3736fe246f52761688d7166e
文蔺 <angusfu1126@qq.com>
2020-03-06 17:48:15 +0800

    Update notification.js

--- docs/plugins/notification.js
+++ docs/plugins/notification.js
@@ -11,10 +11,12 @@ export default {
           message: `
             <div>
               <div>
-                1. 请使用 <em>vue@2.5.x</em> 或者 <em>vue@2.6.7+</em>。
+                1. 强烈建议使用 <a href="https://clair.lap.360.cn/">新版 Clair 组件库</a>。
+              <div>
+                2. 请使用 <em>vue@2.5.x</em> 或者 <em>vue@2.6.7+</em>。
               </div>
               <div>
-                2. 千万<b>不要</b>使用 <em>vue@2.6.0 - 2.6.6</em>。
+                3. 千万<b>不要</b>使用 <em>vue@2.6.0 - 2.6.6</em>。
               </div>
 
               相关链接：<a href="https://github.com/vuejs/vue/issues/9553">vue/issues/9553</a>
